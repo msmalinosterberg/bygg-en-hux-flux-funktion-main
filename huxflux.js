ca073bb70ad046420c420bfc66065b3a3aed3620
msmalinosterberg <msmalinosterberg@gmail.com>
2021-02-11 11:05:43 +0100

    first commit

---
+++ huxflux.js
@@ -0,0 +1,15 @@
+// Här är filen du ska ändra i! Huxflux-funktionen ska returnera
+// en lista med 7 unika slumpmässiga heltal mellan 1 och 35. När
+// du öppnat projektet i browsern, kan du trycka på "Prova funktionen"
+// för att köra den och "Kör tester på funktionen" för att se om alla
+// krav uppfylls.
+
+const huxflux = () => {
+  // Denna funktion behöver fyllas på med logik så att den returnerar
+  // en lista med 7 unika slumpmässiga heltal mellan 1 och 35!
+  return
+}
+
+export {
+  huxflux
+}
